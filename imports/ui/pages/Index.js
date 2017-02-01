b73d4930dfdc3bad52c19533c4c4a2968b46e57e
Thanh Vuong <thanh@userful.com>
2017-01-31 18:32:58 -0700

    logo

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+// import logo from './logo.svg';
 
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
+      <img src='logo.svg' alt="logo" />
       <h2>Userful Cloud</h2>
       <p>Intro</p>
       <p><a className="btn btn-success" href="http://userful.com" role="button">Go to Userful Website</a></p>
